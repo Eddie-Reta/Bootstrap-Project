25a0c92b5a65cae76031650b88e95aecc33bd8b2
Eduardo Reta Jr <https://github.com/Eddie-Reta>
2021-09-04 08:42:09 -0400

    update

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,16 @@
 $(function (){
     $("[data-toggle='tooltip']").tooltip()
 });
+
+(() => {
+    const nav = document.querySelector(".navbar");
+    const colorChange = document.getElementsByTagName('nav')[0].getAttribute("data-color");
+    console.log(colorChange)
+    window.onscroll = () => {
+        if(window.scrollY > 10) {
+            nav.classList.add(colorChange);
+        } else {
+            nav.classList.remove(colorChange);
+        }
+    }
+})();
