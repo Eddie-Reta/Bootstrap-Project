db3d2e7f3bd05d5e5c25eefb41381ac42418d53a
Eduardo Reta Jr <https://github.com/Eddie-Reta>
2021-09-09 12:09:59 -0400

    Added scripts to minified deployment code.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,23 +1,43 @@
+//tooltips toggle for reserve button 
 // $(function (){
 //     $("[data-toggle='tooltip']").tooltip()
 // });
+//es6 function on making sticky navbar change color at a certain point on the screen.
+// (() => {
+//     const nav = document.querySelector(".navbar");
+//     const colorChange = document.getElementsByTagName('nav')[0].getAttribute("data-color");
+//     console.log(colorChange)
+//     window.onscroll = () => {
+//         if(window.scrollY > 10) {
+//             nav.classList.add(colorChange);
+//         } else {
+//             nav.classList.remove(colorChange);
+//         }
+//     }
+// })();
+// Had to clear out arrow function want working with usemin.
+// $(function(){
+//     $(".carousel").carousel({interval: 2000});
+//     $("#carouselButton").on("click", () => {
+        
+//         const carouselBtn = $("#carouselButton ").children("i").hasClass("fa-pause");
+//         const playBtn = $("#carouselButton").children("i").addClass("fa-play");
+//         const pauseBtn = $("#carouselButton").children("i").addClass("fa-pause");
 
-(() => {
-    const nav = document.querySelector(".navbar");
-    const colorChange = document.getElementsByTagName('nav')[0].getAttribute("data-color");
-    console.log(colorChange)
-    window.onscroll = () => {
-        if(window.scrollY > 10) {
-            nav.classList.add(colorChange);
-        } else {
-            nav.classList.remove(colorChange);
-        }
-    }
-})();
-
+//         if (carouselBtn) {
+//             $(".carousel").carousel("pause");
+//             $("#carouselButton").children("i").removeClass("fa-pause");
+//             playBtn;
+//         } else {
+//             $(".carousel").carousel("cycle");
+//             $(".carouselButton").children("i").removeClass("fa-play");
+//             pauseBtn;
+//         } 
+//     });
+// });
 $(function(){
     $(".carousel").carousel({interval: 2000});
-    $("#carouselButton").on("click", () => {
+    $("#carouselButton").on("click", function() {
         
         const carouselBtn = $("#carouselButton ").children("i").hasClass("fa-pause");
         const playBtn = $("#carouselButton").children("i").addClass("fa-play");
